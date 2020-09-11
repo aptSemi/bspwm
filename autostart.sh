54ad319034aa3235a2ef3eee35ef2a61910d5cf0
Semi <53193236+aptSemi@users.noreply.github.com>
2020-09-10 19:50:50 -0700

    Update autostart.sh

--- autostart.sh
+++ autostart.sh
@@ -52,4 +52,5 @@ run volumeicon &
 
 ### xrandr extra resolution settings ###
 
-xrandr --output DP-0 --mode 1920x1080 --rate 144
+#xrandr --output DP-0 --mode 1920x1080 --rate 144
+xrandr --output DP-0 --mode 2560x1080 --rate 144
